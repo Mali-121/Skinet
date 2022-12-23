9c99c116a66f12780e99dbafae3a201ae185bdc1
Mali-121 <74376646+Mali-121@users.noreply.github.com>
2022-12-24 00:44:35 +0800

    Paging, sorting, filtering has been added now to the client side development to use the apis

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
         }
 
@@ -56,6 +63,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
